059d61d92d41e67c482a536e5e35bf0fb21ae3e4
haoningsmann <56476208+haoningsmann@users.noreply.github.com>
2021-03-12 17:26:29 +0800

    小程序初始化加载云服务

--- program/app.js
+++ program/app.js
@@ -1,6 +1,17 @@
 App({
-  onLaunch (options) {
-    // Do something initial when launch.
+  onLaunch: function () {
+    //初始化
+    if (wx.cloud) {
+      wx.cloud.init({
+        env: 'text-fc9vo',
+        traceUser: true
+      })
+    } else {
+      wx.showModal({
+        content: '您的微信版本过低，可能会导致某些功能无法使用，请将微信更新至最新版本！',
+        showCancel: false
+      })
+    }
   },
   onShow (options) {
     // Do something when show.
@@ -11,10 +22,7 @@ App({
   onError (msg) {
     console.log(msg)
   },
-  globalData: {
-   
-  },
-
+  globalData: 'I am global data'
 },
 wx.cloud.init({
   env:'text-fc9vo',
