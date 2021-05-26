787687c6d2857e3853caf281bfb83b22036f58e1
haoningsmann <56476208+haoningsmann@users.noreply.github.com>
2021-05-26 13:22:34 +0800

    更新云函数

--- cloudfunctions/addBooked/index.js
+++ cloudfunctions/addBooked/index.js
@@ -3,14 +3,12 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
+const db = cloud.database()
+const booked = db.collection("BOOKED")
+
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  return await booked.add({
+    data: event
+  })
 }
